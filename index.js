d1a4a338800ea6c394c99b317e7e807cedd52d05
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 16:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const drivers = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendDriver(name){
   drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+
   drivers.unshift(name);
 }
